66af374d68408eb1ad4e0b61e5c5aa45319c9a31
magestus <magestus@yandex.ru>
2016-12-11 17:21:53 +0500

    Update module1.py

--- module1.py
+++ module1.py
@@ -62,7 +62,6 @@ def main():
     search_text = input('Введите запрос: ').encode('utf8')
 
     parse(get_html('https://www.fabrikant.ru/trades/procedure/search/?type=1&query={}&procedure_stage=1&price_from=&price_to=&currency=0&date_type=date_publication&date_from=&date_to=&ensure=all&section_type%5B%5D=ds300&count_on_page=10&order_by=default&order_direction=1'.format(urllib.parse.quote(search_text))))
-#11
 
 
 if __name__ == "__main__":
